ce683d110c5fdc52caf5abb855872648829402cd
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-07-22 01:46:24 +0200

    Add greet IO function

--- functions.hs
+++ functions.hs
@@ -4,3 +4,8 @@ doubleUs x y = doubleMe x + doubleMe y
 
 isEven :: Int -> Bool
 isEven x = (mod x 2) == 0
+
+greet = do  
+    putStrLn "What is your name?"  
+    name <- getLine  
+    putStrLn ("Hello " ++ name ++ "!") 
