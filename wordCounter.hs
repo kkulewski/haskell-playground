a9aa02e90bcb403b44e25241e3d32a0090968786
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-07-22 02:37:15 +0200

    Add type declarations to wordCounter

--- wordCounter.hs
+++ wordCounter.hs
@@ -1,7 +1,10 @@
+splitLine :: [Char] -> [[Char]]
 splitLine line = words line
 
+countWords :: [Char] -> Int
 countWords line = length (splitLine line)
 
+getWordCount :: [Char] -> [Char]
 getWordCount input = show (countWords input)
 
 main = interact getWordCount
