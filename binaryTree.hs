12c683e93c2760f13a50a6c42d20f83a8316fe12
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-07-23 01:16:23 +0200

    Add simple example tree

--- binaryTree.hs
+++ binaryTree.hs
@@ -1,3 +1,9 @@
 data Tree x = Node x (Tree x) (Tree x)
             | Empty
             deriving (Show)
+
+
+tree1 = 
+    Node "Parent" 
+        (Node "Left child" Empty Empty)
+        (Node "Right child" Empty Empty)
