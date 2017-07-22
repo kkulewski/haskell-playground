d5d10a8c2f4715830b0ef79191febb756cb8ac8a
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-07-23 01:33:03 +0200

    Add more complex tree example

--- binaryTree.hs
+++ binaryTree.hs
@@ -7,3 +7,22 @@ tree1 =
     Node "Parent" 
         (Node "Left child" Empty Empty)
         (Node "Right child" Empty Empty)
+
+
+tree2 =
+    (Node 10
+        (Node 5
+            (Node 2 Empty Empty)
+            (Node 7 
+                (Node 6 Empty Empty)
+                Empty
+            )
+        )
+        (Node 15
+            (Node 12 Empty Empty)
+            (Node 18
+                (Node 16 Empty Empty)
+                (Node 20 Empty Empty)
+            )
+        )
+    )
