b9cfce6f93a2695bcb8bb4b231b1b83a5514cf6a
Krzysztof Kulewski <krzysztofkulewski@gmail.com>
2017-07-27 00:30:10 +0200

    Add file processing function

--- processFile.hs
+++ processFile.hs
@@ -1,5 +1,11 @@
+import System.Environment (getArgs)
+
 applyFunction function inputFile outputFile = do
     input <- readFile inputFile
     writeFile outputFile (function input)
 
-
+processFile function = do
+    args <- getArgs
+    case args of
+        [input, output] -> applyFunction function input output
+        _ -> putStrLn "Error - two args expected."
